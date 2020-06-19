2295497a720cf685dd1f93b43aa76ef61c6b5224
Polina Dudchenko <pv.dudchenko@gmail.com>
2020-06-19 11:26:02 +0200

    Main method was created

--- integer-division/src/main/java/com/dudchenko/Main.java
+++ integer-division/src/main/java/com/dudchenko/Main.java
@@ -1,5 +1,19 @@
 package com.dudchenko;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+	
+		System.out.println("Let's make a division! Type a dividend");
+		int dividend = scanner.nextInt();
+		System.out.println("Type a divisor, cannot be zero");
+		int divisor = scanner.nextInt();
+	
+		Division division = new Division();
+		String result = division.makeDivision(dividend, divisor);
+		System.out.println(result);
+	}
 }
